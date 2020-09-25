aa865cb3b9917fd927e831c1d6d692b178a260f0
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2020-09-25 13:32:07 +0200

    Added script to upload dist folder to Azure

---
+++ scripts/azure.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SAS_TOKEN=$1
+
+tar -cvf ../dist.tar ../dist
+
+
+curl -X PUT -T ../dist.tar -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://swarmstoraccount.blob.core.windows.net/oslo-standards-register/dist.tar?$SAS_TOKEN"
