84cb1d95d7261070641aba09b96829418e34f830
Joeri Moreno <morenojoeri@hotmail.com>
2020-12-11 17:55:44 +0100

    make sure dist only contains the files

--- scripts/azure.sh
+++ scripts/azure.sh
@@ -2,7 +2,7 @@
 
 SAS_TOKEN=$1
 
-tar -cvf ../dist.tar ../dist
+tar -cvf ../dist.tar *
 
 
 curl -X PUT -T ../dist.tar -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://swarmstoraccount.blob.core.windows.net/oslo-standards-register/dist.tar?$SAS_TOKEN"
