b4cdbf43f243eddd18ae0e762d5c88355e580d3a
Joeri Moreno <morenojoeri@hotmail.com>
2021-05-28 18:38:06 +0200

    update so that push to production is possible

--- scripts/azure.sh
+++ scripts/azure.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 SAS_TOKEN=$1
+URL=$2
 
 tar -cvf ../dist.tar *
 
 
-curl -X PUT -T ../dist.tar -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "https://swarmstoraccount.blob.core.windows.net/oslo-standards-register/dist.tar?$SAS_TOKEN"
+curl -X PUT -T ../dist.tar -H "x-ms-date: $(date -u)" -H "x-ms-blob-type: BlockBlob" "$URL?$SAS_TOKEN"
